6a21cc61efe43efe88e77c5c3903dd5e320f0df5
winstencoellins <winstencoellins13@gmail.com>
2025-05-03 12:35:40 +0700

    fix: deployment error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "no-var": "off",
-      "@typescript-eslint/no-unused-vars": "off"
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off"
     }
   }
 ];
